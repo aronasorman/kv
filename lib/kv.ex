ff1c27885e4ad6f68da904738fe43364d09e0819
Aron Fyodor Asor <aronasorman@gmail.com>
2014-02-13 22:27:02 -0800

    start and stop dependency applications

--- lib/kv.ex
+++ lib/kv.ex
@@ -4,6 +4,16 @@ defmodule Kv do
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
   def start(_type, _args) do
+    :ok = :application.start :crypto
+    :ok = :application.start :ranch
+    :ok = :application.start :cowlib
+    :ok = :application.start :cowboy
     Kv.Supervisor.start_link
+  end
+
+  def stop(_state) do
+    :application.stop :cowboy
+    :application.stop :ranch
+    :application.stop :cow_lib
   end
 end
